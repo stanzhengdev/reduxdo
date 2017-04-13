c2a61e9104b4a0ff824f938d311127bc0a05cd55
Stanley Zheng <dev.stanzheng@gmail.com>
2017-04-13 14:05:17 -0400

    clean up old directory

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import reducer from "./reducers";
+import "./App.css";
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
