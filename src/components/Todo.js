88a4d4fe3720ab0c02cf49eafd95197101cfb679
Stanley Zheng <dev.stanzheng@gmail.com>
2017-04-13 16:28:55 -0400

    fix create-react-type

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
-import React, { PropTypes } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { removeTodo } from "../actions";
+import PropTypes from "prop-types";
 
 const Todo = ({ onClick, onTodoRemoveClick, completed, text }) => (
   <li
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
-import React, { PropTypes } from "react";
+import React from "react";
 import Todo from "./Todo";
+import PropTypes from "prop-types";
 
 const TodoList = ({ todos, onTodoClick, onTodoRemoveClick }) => (
   <ul className="todo-list">
