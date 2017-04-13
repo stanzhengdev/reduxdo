2a050760028693a9f2861ac03d1aed166fe4c98f
Stanley Zheng <dev.stanzheng@gmail.com>
2017-04-13 14:06:39 -0400

    prettier lint

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,23 @@
-import React, { PropTypes } from 'react'
-import Todo from './Todo'
+import React, { PropTypes } from "react";
+import Todo from "./Todo";
 
 const TodoList = ({ todos, onTodoClick }) => (
   <ul>
-    {todos.map(todo =>
-      <Todo
-        key={todo.id}
-        {...todo}
-        onClick={() => onTodoClick(todo.id)}
-      />
-    )}
+    {todos.map(todo => (
+      <Todo key={todo.id} {...todo} onClick={() => onTodoClick(todo.id)} />
+    ))}
   </ul>
-)
+);
 
 TodoList.propTypes = {
-  todos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    completed: PropTypes.bool.isRequired,
-    text: PropTypes.string.isRequired
-  }).isRequired).isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
   onTodoClick: PropTypes.func.isRequired
-}
+};
 
-export default TodoList
+export default TodoList;
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,39 +1,34 @@
 const todo = (state, action) => {
   switch (action.type) {
-    case 'ADD_TODO':
+    case "ADD_TODO":
       return {
         id: action.id,
         text: action.text,
         completed: false
-      }
-    case 'TOGGLE_TODO':
+      };
+    case "TOGGLE_TODO":
       if (state.id !== action.id) {
-        return state
+        return state;
       }
 
       return {
         ...state,
         completed: !state.completed
-      }
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
 const todos = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(t =>
-        todo(t, action)
-      )
+    case "ADD_TODO":
+      return [...state, todo(undefined, action)];
+    case "TOGGLE_TODO":
+      return state.map(t => todo(t, action));
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default todos
+export default todos;
